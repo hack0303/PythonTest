e932c1f8f72a3f625fc689bcb2415650a8933436
hack0303 <1097037585@qq.com>
2018-12-24 18:33:48 +0800

    Link:https://docs.python.org/3/tutorial/controlflow.html 函数定义相关

--- com/creating/www/main/controlFlowTools/DefFunctionTest.py
+++ com/creating/www/main/controlFlowTools/DefFunctionTest.py
@@ -4,6 +4,7 @@ Created on 2018��12��24��
 
 @author: developorName
 '''
+from pip._vendor.six import assertCountEqual
 '''
 注意定义方法放的域
 '''
@@ -53,6 +54,30 @@ def test005(a,sequence=None):
         sequence=[]
         sequence.append(a)
     print(sequence)
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+def testParam(param01,*param02,**param03):
+    print(param01)
+    for x in param02:
+        print(x)
+    for key in param03:
+        print(key+":"+param03[key])
+def concat(*args,seq='.'):
+    print(seq.join(args))
+def testabc(a,b='b',c='c',seq='.'):
+    lst=[]
+    lst.append(a)
+    lst.append(b)
+    lst.append(c)
+    print(seq.join(lst))
+def testabc_multiple(**args):
+    for x in args:
+        print("key:"+x+":"+args[x])
+def testlambda(n):
+    return lambda x:x+n
 if __name__ == '__main__':
     func01(10)
     func02(10)
@@ -84,4 +109,39 @@ if __name__ == '__main__':
     test005(2)
     test005(3)
     #总结同上，基本类型与对象引用里的区别
+    #以下LINK：https://docs.python.org/3/tutorial/controlflow.html，都是直接复制黏贴
+    parrot(1000)                                          # 1 positional argument
+    parrot(voltage=1000)                                  # 1 keyword argument
+    parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
+    parrot(action='VOOOOOM', voltage=1000000)             # 2 keyword arguments
+    parrot('a million', 'bereft of life', 'jump')         # 3 positional arguments
+    parrot('a thousand', state='pushing up the daisies')  # 1 positional, 1 keyword
+    #parrot()                     # required argument missing
+    #parrot(voltage=5.0, 'dead')  # non-keyword argument after a keyword argument
+    #parrot(110, voltage=220)     # duplicate value for the same argument
+    #parrot(actor='John Cleese')  # unknown keyword argument
+    '可变数量参数方法 Arbitrary Argument Lists'
+    testParam('111')
+    testParam('111','xxx','yyy',
+              key='hehe',key02='haha')
+    concat('a','b','c')
+    concat('a','b','c',seq='x')
+    #concat('a','b',seq='x','c')#error
+    'Unpacking Argument Lists'
+    print(list(range(3,6)))
+    lst=[3,6]
+    print(list(range(*lst)))
+    #测试可见，拆包用的序列中的前两个值
+    lst=[3,4,6]
+    print(list(range(*lst)))
+    #注意解包符号
+    testabc(**{'a':'a','b':'b','c':'c'})
+    testabc_multiple(**{'a':'a','b':'b','c':'c','d':'d'})
+    #测试返回值为lambda
+    f=testlambda(100)
+    print(f(1))
+    #测试参数为lambda,按照元组中第二个值作为Key
+    pairs=[(1,'one'),(2,'two'),(3,'three'),(4,'four')]
+    pairs.sort(key=lambda pair:pair[1])
+    print(pairs)
     pass
