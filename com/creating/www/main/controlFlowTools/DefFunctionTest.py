4230d6dbcd7a6b58f5a38273c7e18bee67f1e8db
hack0303 <1097037585@qq.com>
2018-12-24 16:24:33 +0800

    void or type return

--- com/creating/www/main/controlFlowTools/DefFunctionTest.py
+++ com/creating/www/main/controlFlowTools/DefFunctionTest.py
@@ -7,8 +7,23 @@ Created on 2018��12��24��
 '''
 注意定义方法放的域
 '''
-def func(x):
+def func01(x):
     print(list(range(x)))
+#无值返回
+def func02(param):
+    for x in range(param):
+        if x==param-1:
+            print(x,end='\n')
+        else:
+            print(x,end=' ')
+#有值返回
+def func03(param):
+    result=[]
+    for x in range(param):
+        result.append(x)
+    return result
 if __name__ == '__main__':
-    func(100)
+    func01(10)
+    func02(10)
+    print(func03(10))
     pass
