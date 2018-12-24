1fa0e461bf2ce46793540eeb12c4602febbc8eb3
hack0303 <1097037585@qq.com>
2018-12-24 12:57:10 +0800

    Python test first commit

---
+++ Example001.py
@@ -0,0 +1,46 @@
+'''
+Created on 2018骞�10鏈�17鏃�
+
+@author: Administrator
+'''
+from builtins import str
+
+if __name__ == '__main__':
+    pf=pow(2,2)
+    jdz=abs(-1)
+    a=round(2.3)
+    b=round(2.5)
+    c=round(3.144,2)
+    d=round(3.145,2)
+    _min=min(1,2,3)
+    _max=max(1,2,3)
+    print("%d",pf)
+    1+1
+    2-1
+    1*1
+    1/2
+    1**2
+    1//2
+    1%2
+    'hello everyonee'
+    print("i'm learning python")
+    print('"呵呵"')
+    print("xman "*5)
+    print("x" in "xman")
+    str="12345"
+    print(str[0])
+    print(str[len(str)-1])
+    print(str[-1])
+    print(str[:])
+    print(str[:len(str)])
+    print(str[0:-1])#1234
+    print("abc".upper())
+    print("ABC".lower())
+    print("not \\n",end="$")
+    print("en")
+    print("a","b","c",sep="#")
+    print("abc"=="abcd")
+    print("abc"<"abcd")#比较相等，长度不一，比较长度
+    print(format("aaa","5s"),format("aaa","2s"),format("aaa",">5s"),sep="|")
+    
+    pass
