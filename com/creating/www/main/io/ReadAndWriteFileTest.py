2626b51898587204e07848d19a646dd1adf2c5f6
hack0303 <1097037585@qq.com>
2018-12-26 10:02:28 +0800

    IO test

--- com/creating/www/main/io/ReadAndWriteFileTest.py
+++ com/creating/www/main/io/ReadAndWriteFileTest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf8 -*-
 '''
-Created on 2018��12��25��
+Created on 2018年12月25日
 
 @author: developorName
 '''
@@ -44,4 +44,31 @@ if __name__ == '__main__':
     f.close()
     print()
     print('-'*40)
+    with open('resources/test.txt') as f:
+        line=f.readline()
+        print(f.tell())
+        print(line,end=' ')
+        print(f.seek(0))
+        line=f.readline()
+        print(line,end=' ')
+        print(f.seek(3))
+        line=f.readline()
+        print(line,end=' ')
+        #print(f.seek(100))
+        #print(f.seek(4097))
+        print(f.seek(3,0))
+        line=f.readline()
+        print(line,end=' ')
+        print(f.tell())
+    f.close()
+    import json
+    print(json.dumps([1,'哈哈','e']))#查看dump后的结果形式
+    with open('resources/dump.txt','w') as f:#序列化
+        json.dump([1,'hehe','嘿嘿'],f)
+    f.close()
+    x=None
+    with open('resources/dump.txt') as f:#反序列化 
+        x=json.load(f)
+    f.close()
+    print(x)
     pass
